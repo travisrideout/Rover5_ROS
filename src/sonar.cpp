d4446a6ac34f2eb21645361e129db32f37d0419d
travisrideout <travis.rideout518@gmail.com>
2015-07-06 19:09:00 -0400

    got costmap working

--- src/sonar.cpp
+++ src/sonar.cpp
@@ -18,8 +18,16 @@ void Sonar::PingCallback(const Rover5_ROS::rover_out::ConstPtr& msg){
 	range_msg.radiation_type = 0;
 	range_msg.field_of_view = 0.1;
 	range_msg.min_range = 0.01;
-	range_msg.max_range = 300;
+	range_msg.max_range = 3.0;
 	range_msg.range = msg->pingDist/100.0;	//convert from cm to meters
+
+	//Set values outside max/min setting to those values, used to clear sensor cone in costmap
+	//error readings, greater than 30m ignored
+	if(range_msg.range>range_msg.max_range && range_msg.range<30.0){
+		range_msg.range = range_msg.max_range;
+	}else if(range_msg.range<range_msg.min_range){
+		range_msg.range = range_msg.min_range;
+	}
 
 	range_pub.publish(range_msg);
 
